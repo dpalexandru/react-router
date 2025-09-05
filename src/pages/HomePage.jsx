868b106ac929d93e99611ea131668af9950491e1
dpalexandru <alexpoian91@hotmail.com>
2025-09-05 15:24:40 +0200

    feat: creata a hompage page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='container'>
+      <div className="row">
+        <div className="col-6 col-md-12">
+          <img src="https://t4.ftcdn.net/jpg/01/15/04/39/360_F_115043913_g00I2WhOKYresf7JId9GTTnNy50FBDRi.jpg" alt="" />
+          <h4>Oggi saldi su Fake store non perdere l'occasione! Vedi qui i prodotti!
+          </h4>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default HomePage
