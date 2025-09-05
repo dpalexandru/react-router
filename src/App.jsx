f58e54677fb59b9ea1c27fb09726628a7139d8e1
dpalexandru <alexpoian91@hotmail.com>
2025-09-05 15:40:29 +0200

    feat: create a route for /chisiamo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
+import ChiSiamo from "./pages/ChiSiamo"
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/chisiamo" element={<ChiSiamo />} />
     </Routes>
   )
 }
---
+++ src/pages/ChiSiamo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ChiSiamo = () => {
+  return (
+    <div className="container mt-5">
+      <h2 className="mb-4">Chi Siamo</h2>
+      <p className="lead">
+        Siamo la <strong>Classe 147</strong>, un gruppo di studenti pieni di entusiasmo che ha deciso di mettersi alla prova creando un <em>Fake Store</em>.
+        La nostra forza è l’unione e la voglia di imparare: ognuno di noi contribuisce con idee, creatività e impegno.
+        Siamo convinti di essere la classe migliore, perché non ci fermiamo davanti alle difficoltà e crediamo che, passo dopo passo, <strong>ci riusciremo tutti</strong>!
+      </p>
+    </div>
+  )
+}
+
+export default ChiSiamo
