367465fcd93b691fa266fcdfc98a063d0505fbc7
dpalexandru <alexpoian91@hotmail.com>
2025-09-05 15:48:22 +0200

    feat: create a route for/prodotti

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import ChiSiamo from "./pages/ChiSiamo"
+import Prodotti from "./pages/Prodotti"
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/chisiamo" element={<ChiSiamo />} />
+      <Route path="/prodotti" element={<Prodotti />} />
     </Routes>
   )
 }
---
+++ src/pages/Prodotti.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Prodotti = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <h1>Qui i prodotti:</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Prodotti
