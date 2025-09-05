62b3a2cb182e0b0ae42f612221f5fd6872bac49d
dpalexandru <alexpoian91@hotmail.com>
2025-09-05 15:56:37 +0200

    feat: show in console product data

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+// link recupero dati
+const url = "https://fakestoreapi.com/products"
 
 const Prodotti = () => {
+
+  //recupero i dati dei prodotti tramite chiama axios
+  useEffect(() => {
+    axios.get(url)
+      .then(resp => {
+        console.log(resp.data)
+      })
+  }, []
+  )
+
+
   return (
     <div className="container">
       <div className="row">
