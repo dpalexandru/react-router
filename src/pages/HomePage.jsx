0e4679d517790ec6f9f4f2b55876d67a100692cf
dpalexandru <alexpoian91@hotmail.com>
2025-09-05 15:36:08 +0200

    fix: homepage style

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,15 +2,27 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className='container'>
-      <div className="row">
-        <div className="col-6 col-md-12">
-          <img src="https://t4.ftcdn.net/jpg/01/15/04/39/360_F_115043913_g00I2WhOKYresf7JId9GTTnNy50FBDRi.jpg" alt="" />
-          <h4>Oggi saldi su Fake store non perdere l'occasione! Vedi qui i prodotti!
-          </h4>
-        </div>
+    <div className="container mt-5">
+      <div className="text-center mb-4">
+        <h1 className="display-5 fw-bold">Benvenuto!</h1>
       </div>
 
+      <div className="row align-items-center">
+        <div className="col-md-6 text-center mb-3 mb-md-0">
+          <img
+            src="https://t4.ftcdn.net/jpg/01/15/04/39/360_F_115043913_g00I2WhOKYresf7JId9GTTnNy50FBDRi.jpg"
+            alt="saldi"
+            className="img-fluid rounded shadow"
+          />
+        </div>
+        <div className="col-md-6">
+          <h4 className="fw-semibold">Oggi saldi su Fake Store!</h4>
+          <p className="lead">
+            Non perdere l'occasione! Dai un’occhiata ai nostri prodotti in super sconto e approfitta delle offerte di oggi.
+          </p>
+          <button className="btn btn-primary btn-lg">Vedi Prodotti</button>
+        </div>
+      </div>
     </div>
   )
 }
