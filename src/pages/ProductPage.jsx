fcde556aafae0fabe45371bd6c809e5f3cbaa08a
dpalexandru <alexpoian91@hotmail.com>
2025-09-08 16:14:47 +0200

    feat: show dinamic product info

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
 import { Card, Button } from "react-bootstrap";
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 
 const ProductPage = () => {
@@ -16,29 +17,44 @@ const ProductPage = () => {
     axios.get(`https://fakestoreapi.com/products/${id}`)
       .then(res => {
         setProduct(res.data);
-        console.log(product)
       })
   }, [id])
 
 
   return (
-    <div>
-      <div className="container py-4 section-light">
-        <Card className="shadow-sm" style={{ maxWidth: 420 }}>
-          <Card.Img
-            variant="top"
-            src="dddd"
-            alt="Placeholder"
-          />
-          <Card.Body>
-            <Card.Title>{product.title}</Card.Title>
-            <Card.Text>
-            </Card.Text>
-            <Button variant="primary">Action</Button>
-          </Card.Body>
-        </Card>
-      </div>
+    <div className="container py-4 section-light d-flex justify-content-center">
+      <Card className="shadow-sm">
+        <div className="row">
+          <div className="col-md-4">
+            <Card.Img
+              className="img-fluid h-100"
+              src={product.image}
+              alt={product.title}
+              style={{ objectFit: "contain", padding: "1rem" }}
+            />
+          </div>
+          <div className="col-md-8">
+            <Card.Body>
+              <Card.Title>{product.title}</Card.Title>
+              <Card.Text className="text-muted">
+                {product.category}
+              </Card.Text>
+              <Card.Text>
+                <strong>Description: </strong>{product.description}
+              </Card.Text>
+              <Card.Text className="fw-bold text-danger">Price:  € {product.price}</Card.Text>
+              <Button as={Link} to="/prodotti" variant="primary">
+                Vai ai Prodotti
+              </Button>
+              <Button disabled variant="danger m-3">Buy</Button>
+
+            </Card.Body>
+          </div>
+        </div>
+      </Card>
     </div>
+
+
   )
 }
 
