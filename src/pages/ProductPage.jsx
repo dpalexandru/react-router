b63f623efe5aa87365b62964e610ba390503e8f6
dpalexandru <alexpoian91@hotmail.com>
2025-09-08 15:07:18 +0200

    feat: add a static page of a single product

---
+++ src/pages/ProductPage.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { Card, Button } from "react-bootstrap";
+
+const ProductPage = () => {
+  return (
+    <div>
+      <div className="container py-4">
+        <Card className="shadow-sm" style={{ maxWidth: 420 }}>
+          <Card.Img
+            variant="top"
+            src="https://picsum.photos/640/360"
+            alt="Placeholder"
+          />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+            </Card.Text>
+            <Button variant="primary">Action</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  )
+}
+
+export default ProductPage
